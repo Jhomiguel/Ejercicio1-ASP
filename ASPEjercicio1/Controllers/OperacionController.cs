ef8f6bb31f3f42b6bbb3c2b5549b390dd2f60a7a
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-02-16 19:26:59 -0400

    Commit 1

---
+++ ASPEjercicio1/Controllers/OperacionController.cs
@@ -0,0 +1,26 @@
+﻿using ASPEjercicio1.Models;
+using System.Web.Mvc;
+
+namespace ASPEjercicio1.Controllers
+{
+    public class OperacionController : Controller
+    {
+        // GET: Operation
+        public ActionResult Index()
+        {
+            return View(new operacion());
+        }
+        [HttpPost]
+        public ActionResult Index(operacion num)
+        {
+            string numS = num.numero.ToString();
+            char[] c = new char[2];
+            c[0] = numS[1];
+            c[1] = numS[0];
+            string result = new string(c);
+            num.numero =int.Parse(result);
+            return View(num);  
+        }
+
+    }
+}
---
+++ ASPEjercicio1/Models/operacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace ASPEjercicio1.Models
+{
+    public class operacion
+    {
+       public int numero { get; set; }
+       
+
+        public static int InvertirNum(int num)
+        { 
+                string numS = num.ToString();
+                char[] c = new char[2];
+                c[0] = numS[1];
+                c[1] = numS[0];
+                string result = new string(c);      
+                return int.Parse(result);
+        }
+    }
+}
